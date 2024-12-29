e82230447258d0cc9f44792366f254a3319524eb
nancynewlin-masi <86989160+nancynewlin-masi@users.noreply.github.com>
2024-12-28 19:10:55 -0600

    get small model (~3 GB) and launch thru terminal

--- SetupLLM_locally.sh
+++ SetupLLM_locally.sh
@@ -1,4 +1,13 @@
 # Nancy Newlin, 12/28/24 
 # Using instructions from https://github.com/Mozilla-Ocho/llamafile
+# Get from online
+wget https://huggingface.co/Mozilla/Llama-3.2-1B-Instruct-llamafile/resolve/main/Llama-3.2-1B-Instruct.Q6_K.llamafile
 
+# Change permissions of file
+chmod +x Llama-3.2-1B-Instruct.Q6_K.llamafile
+
+# Start server
+./Llama-3.2-1B-Instruct.Q6_K.llamafile
+
+# End server with Ctrl+C
 
